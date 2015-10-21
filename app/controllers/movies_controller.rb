0841c914a32ff3ae856f4b7b46580a5ab502f77c
郑道毅 <1174102864@qq.com>
2015-10-21 22:57:33 +0800

    hm 3 part 2 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,30 @@ class MoviesController < ApplicationController
   end
 
   def index
+    sort = params[:sort] || session[:sort] || {}
+    if sort == 'title'
+      ordering, @title_header = {:order => :title}, 'hilite'
+    elsif sort =='release_date'
+      ordering, @date_header = {:order => :release_date}, 'hilite'
+    end
     @movies = Movie.all
+    @all_ratings = Movie.all_ratings
+    @selected_ratings = params[:ratings] || session[:ratings] || {}
+    if @selected_ratings == {}
+      @selected_ratings = Hash[@all_ratings.map {|rating| [rating, rating]}]
+    end
+    if params[:sort] != session[:sort]
+      session[:sort] = sort
+    end
+    if params[:ratings] != session[:ratings] and @selected_ratings != {}
+      session[:sort] = sort
+      session[:ratings] = @selected_ratings
+    end
+    if sort != {}
+      @movies = Movie.where("rating": @selected_ratings.keys).order(ordering[:order])
+    else
+      @movies = Movie.where("rating": @selected_ratings.keys)
+    end
   end
 
   def new
